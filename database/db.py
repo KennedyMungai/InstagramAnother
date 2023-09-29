f790e3b5ef84c831b3e74762e13e6009738d1083
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-29 03:02:31 +0300

    The database connection logic

---
+++ database/db.py
@@ -0,0 +1,35 @@
+"""The database file"""
+import os
+
+import pymysql
+from dotenv import find_dotenv, load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+find_dotenv(load_dotenv())
+
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+MYSQL_USER = os.environ.get("MYSQL_USER")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_PORT = os.environ.get("MYSQL_PORT")
+
+engine = create_engine(
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+def get_db():
+    """The function to get the database instance
+
+    Yields:
+        _db: The database instance
+    """
+    _db = SessionLocal()
+    try:
+        yield _db
+    finally:
+        _db.close()
