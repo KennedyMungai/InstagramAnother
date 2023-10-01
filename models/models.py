93534e97758f8437712088136ec5afb8346bf314
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-01 16:47:38 +0300

    Used proper cinfig

--- models/models.py
+++ models/models.py
@@ -12,6 +12,6 @@ class DbUser(Base):
     """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    email = Column(String)
-    password = Column(String)
+    username = Column(String(255))
+    email = Column(String(255))
+    password = Column(String(255))
